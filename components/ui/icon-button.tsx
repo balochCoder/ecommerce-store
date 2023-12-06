eb9d97b436371bb773c3f99c888fa67be3536560
Raheel <raheel@lubmnetwork.local>
2023-12-06 18:50:07 +0400

    first commit

---
+++ components/ui/icon-button.tsx
@@ -0,0 +1,24 @@
+import React, { MouseEventHandler } from "react";
+import { cn } from "@/lib/utils";
+
+interface IconButtonProps {
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  className?: string;
+  icon: React.ReactElement;
+}
+
+const IconButton = ({ onClick, className, icon }: IconButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
+        className
+      )}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default IconButton;
